fe06c565dad75d9254fc049d9edbbe47c2f1e044
FirstFlush <firstflush@protonmail.com>
2026-01-17 21:29:31 -0800

    fix: remove redundent logging config from settings

--- garden/config/settings.py
+++ garden/config/settings.py
@@ -1,10 +1,5 @@
-import logging
-import logging.config
 from pathlib import Path
-from .logging import LOGGING
 
-logging.config.dictConfig(LOGGING)
-logger = logging.getLogger(__name__)
 
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 APP_DIR = ROOT_DIR / "garden"
