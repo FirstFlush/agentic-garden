2450e2f3fbc4545c4d75ab91b4303a80adfd577a
FirstFlush <firstflush@protonmail.com>
2026-01-17 18:38:17 -0800

    chore: main scaffolding

--- garden/cli/main.py
+++ garden/cli/main.py
@@ -2,21 +2,29 @@ import logging
 import logging.config
 import typer
 from dotenv import load_dotenv
-from garden.config.logging import LOGGING
+from ..config.logging import LOGGING
+from ..storage.db import db
+from ..storage.models import RawObservation
 
 logger = logging.getLogger(__name__)
 app = typer.Typer(add_completion=False)
 
 
+def init_db():
+    db.connect(reuse_if_open=True)
+    db.create_tables([RawObservation], safe=True)
+    logger.info("Established DB connection")
+
+
 def bootstrap():
     load_dotenv()
     logging.config.dictConfig(LOGGING)
+    init_db()
     logger.info("CLI bootstrap complete")
 
 
 @app.command()
 def test(name: str = "world"):
-    logger = logging.getLogger(__name__)
     logger.info("test command invoked")
     typer.echo(f"hello {name}")
 
